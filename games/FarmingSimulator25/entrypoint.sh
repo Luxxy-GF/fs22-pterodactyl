29b049c735fffef171c5d3088dd0d4e63e45e25a
Luxxy <68338577+Luxxy-GF@users.noreply.github.com>
2024-11-15 18:59:16 +0000

    e

--- games/FarmingSimulator25/entrypoint.sh
+++ games/FarmingSimulator25/entrypoint.sh
@@ -64,8 +64,5 @@ echo "✓ started Nginx..."
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-export WINEPREFIX=~/fs25-wine
-WINEARCH=win64 winetricks
-
 # Run the Server
 eval ${MODIFIED_STARTUP}

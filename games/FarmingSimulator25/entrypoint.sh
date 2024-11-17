ce2f61c50c91edc91bbc8d21a05fbc12f59ee846
Luxxy <68338577+Luxxy-GF@users.noreply.github.com>
2024-11-17 18:49:34 +0000

    Update entrypoint.sh for Farming Simulator 25: modify VNC server startup command to use custom xstartup script for improved configuration

--- games/FarmingSimulator25/entrypoint.sh
+++ games/FarmingSimulator25/entrypoint.sh
@@ -69,7 +69,7 @@ fi
 
 # Handle various progression states
 if [ "${PROGRESSION}" == "INSTALL_SERVER" ]; then
-    /usr/bin/vncserver -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
+    /usr/bin/vncserver -xstartup /home/container/.vnc/xstartup -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
      # Check if the directory is writable and the file exists
     if [ "1" == "1" ]; then
         echo "You have write permission to the /fs directory and the file the server files seems to exists."
@@ -80,7 +80,7 @@ if [ "${PROGRESSION}" == "INSTALL_SERVER" ]; then
         STARTCMD="sleep 50"
     fi
 elif [ "${PROGRESSION}" == "INSTALL_DLC" ] && [ ! -z "${DLC_EXE}" ]; then
-    /usr/bin/vncserver -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
+    /usr/bin/vncserver -xstartup /home/container/.vnc/xstartup -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
     STARTCMD="wine /home/container/dlc_install/${DLC_EXE}"
 elif [ "${PROGRESSION}" == "SETUP_VNC" ]; then
     # Set up VNC configuration if it doesn't already exist
@@ -100,19 +100,19 @@ elif [ "${PROGRESSION}" == "SETUP_VNC" ]; then
 elif [ "${PROGRESSION}" == "ACTIVATE" ] && [ -f "/home/container/.vnc/passwd" ]; then
     # Activate VNC and set the start command for the game
     echo "Activating VNC server..."
-    /usr/bin/vncserver -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
+    /usr/bin/vncserver -xstartup /home/container/.vnc/xstartup -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
     STARTCMD="wine /home/container/Farming\ Simulator\ 20${FS_VERSION}/FarmingSimulator20${FS_VERSION}.exe"
 
 elif [ "${PROGRESSION}" == "RUN" ] && [ -f "/home/container/.vnc/passwd" ]; then
     # Prepare the startup command using environment variables
     echo "Preparing startup command..."
-    /usr/bin/vncserver -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
+    /usr/bin/vncserver -xstartup /home/container/.vnc/xstartup -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
     STARTCMD=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 
 elif [ "${PROGRESSION}" == "UPDATE" ]; then
         # Update the server
         echo "Updating the server..."
-        /usr/bin/vncserver -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
+        /usr/bin/vncserver -xstartup /home/container/.vnc/xstartup -geometry 1920x1080 -rfbport "${VNC_PORT}" -desktop x11
         STARTCMD="wine /home/container/Farming\ Simulator\ 20${FS_VERSION}/FarmingSimulator20${FS_VERSION}.exe"
 
         echo -e "Please stop the server and set the PROGRESSION variable to RUN"
